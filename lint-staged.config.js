de3ecf89845aa2ba206ed3374e4710ca3f639008
MJ <hlmm8645@gmail.com>
2025-03-01 06:59:54 +0300

    step 0

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "frontend/**/*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-    "frontend/**/*.{json,md}": ["prettier --write"],
-    "backend/**/*.py": ["black", "isort"]
-  };
